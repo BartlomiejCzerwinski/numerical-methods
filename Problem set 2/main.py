6d8e4c879fd16663f962441da04a095411299de0
Poooowero <bartek2002@interia.pl>
2023-04-02 12:52:17 +0200

    gaussJordanAlgorithm here

---
+++ Problem set 2/main.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+def loadNumbersFromFile(file_path):
+    matrix = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            row = [float(x) for x in line.split()]
+            matrix.append(row)
+    return np.array(matrix)
+
+def createMatrix(dataMatrix):
+    result = []
+    for i in dataMatrix:
+        result.append(i[0:-1])
+    return np.array(result)
+
+def createVector(dataMatrix):
+    result = []
+    for i in dataMatrix:
+        result.append(i[len(i)-1])
+    return np.array(result)
+
+def divideRow(matrix, vector, rowNumber):
+    matrixSize = len(vector)
+    pivot = matrix[rowNumber][rowNumber]
+    for i in range(matrixSize):
+        matrix[rowNumber][i] /= pivot
+    vector[rowNumber] /= pivot
+    return matrix, vector
+
+def eliminateRow(matrix, vector,  rowNumber):
+    matrixSize = len(vector)
+    matrixCopy = np.copy(matrix)
+    for i in range(matrixSize):
+        for j in range(matrixSize):
+            if i != rowNumber:
+                matrix[i][j] = matrix[i][j] - (matrixCopy[i][rowNumber] * matrix[rowNumber][j])
+        if i != rowNumber:
+            vector[i] = vector[i] - (matrixCopy[i][rowNumber] * vector[rowNumber])
+
+    return matrix, vector
+
+def swapRowsIfDiagonalZero(matrix, vector, startPosition):
+    n = len(matrix)
+    for i in range(startPosition, n):
+        if matrix[i][i] == 0:
+            for j in range(i+1, n):
+                if matrix[j][i] != 0:
+                    tmp = np.copy(matrix[i])
+                    matrix[i] = matrix[j]
+                    matrix[j] = tmp
+                    tmp2 = vector[i]
+                    vector[i] = vector[j]
+                    vector[j] = tmp2
+                    return matrix, vector
+    return matrix, vector
+
+def isMatrixIndeterminate(matrix, vector):
+    matrixSize = len(vector)
+    for i in range(matrixSize):
+        if round(sum(matrix[i]), 10) == 0 and round(vector[i], 10) == 0:
+            return True
+    return False
+
+def isMatrixInconsistent(matrix, vector):
+    matrixSize = len(vector)
+    for i in range(matrixSize):
+        if round(sum(matrix[i]), 10) == 0 and round(vector[i], 10) != 0:
+            return True
+    return False
+
+def printResults(vector):
+    for i in range(len(vector)):
+        print("x" + str(i) + " = " + str(round(vector[i], 10)))
+
+def gaussJordanAlgorithm(matrix, vector):
+    size = len(vector)
+    for i in range(size):
+        if isMatrixIndeterminate(matrix, vector):
+            print("NIEOZNACZONY")
+            return
+        if isMatrixInconsistent(matrix, vector):
+            print("SPRZECZNY")
+            return
+        matrix, vector = swapRowsIfDiagonalZero(matrix, vector, i)
+        matrix, vector = divideRow(matrix, vector, i)
+        matrix, vector = eliminateRow(matrix, vector, i)
+    printResults(vector)
+
+
+t = loadNumbersFromFile("data.txt")
+matrix = createMatrix(t)
+vector = createVector(t)
+gaussJordanAlgorithm(matrix, vector)
